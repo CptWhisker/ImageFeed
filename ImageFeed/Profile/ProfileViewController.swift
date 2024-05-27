d727b1246dce2b23f34dd2ce9f368f1172f4e698
Aleksandr Moskovtsev <aleks.moskovtsev@gmail.com>
2024-05-27 22:25:49 +0300

    FEATURE: Add profile screen (basic)

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak var profilePictureImage: UIImageView!
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var socialMediaLabel: UILabel!
+    @IBOutlet weak var profileStatusLabel: UILabel!
+}
