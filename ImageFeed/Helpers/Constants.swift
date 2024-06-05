92fb2599e474244b5fe7e05b0f65d9e1837c3c58
Aleksandr Moskovtsev <aleks.moskovtsev@gmail.com>
2024-06-05 23:39:06 +0300

    FEATURE: Add dedicated file 'Constants.swift' containing project`s constants

---
+++ ImageFeed/Helpers/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Constants {
+    static let accessKey = "Slv1A5HrDkCLUd5XHW3dJyGLiy4PmwOVQgEehDdlidQ"
+    static let secretKey = "lyqNLtFO4cFk63eVP6RtSxStIj7KIO80Bs4tKcO_gRQ"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
